5a6c4cfcea7b03a41b2cb36df85e0047ef02f288
Rob Porter <rob@weeverapps.com>
2018-04-25 09:05:06 -0400

    Added Monad laws tests for Inquiry()

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,7 @@
 import { Inquiry, Fail, Pass } from "./index";
 import * as R from "ramda";
 import "jasmine";
+import { Maybe } from "simple-maybe";
 
 const oldEnough = (a: any) =>
     a.age > 13 ? Pass(["old enough"]) : Fail(["not old enough"]);
@@ -21,12 +22,46 @@ function resolveAfter2Seconds(x: any) {
     });
 }
 
-
 describe("The module", () => {
+
+    // note that while these pass, to use this monad as intended
+    // one should never call Inquiry() directly
+    // this first set of tests take values that aren't of the type expected
+    // @todo convert to type expected
+    it("should satisfy the first monad law of left identity", () => {
+        const g = n => Inquiry(n + 1);
+        const f = n => Inquiry(n * 2);
+
+        // 1. unit(x).chain(f) ==== f(x)
+        const leftIdentity1 = Inquiry(1).chain(f);
+        const leftIdentity2 = f(1);
+
+        expect(leftIdentity1.join()).toEqual(leftIdentity2.join());
+    });
+
+    it("should satisfy the second monad law of right identity", () => {
+
+        // 2. m.chain(unit) ==== m
+        const rightIdentity1 = Inquiry(2).chain(Inquiry);
+        const rightIdentity2 = Inquiry(2);
+
+        expect(rightIdentity1.join()).toEqual(rightIdentity2.join());
+    });
+
+    it("should satisfy the third monad law of associativity", () => {
+        const g = n => Inquiry(n + 1);
+        const f = n => Inquiry(n * 2);
+
+        // 3. m.chain(f).chain(g) ==== m.chain(x => f(x).chain(g))
+        const associativity1 = Inquiry(3).chain(g).chain(f);
+        const associativity2 = Inquiry(3).chain(x => g(x).chain(f));
+
+        expect(associativity1.join()).toEqual(associativity2.join());
+    });
+
     it("should be able to make many checks and run a cohort and return the subject unchanged", () => {
         return (Inquiry as any)
             .of({ name: "test", age: 10, description: "blah" })
-            .informant((x: any) => console.log(x))
             .inquire(oldEnough)
             .inquireP(resolveAfter2Seconds)
             .inquire(findHeight)
