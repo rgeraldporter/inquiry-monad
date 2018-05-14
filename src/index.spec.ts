578802b6047a4b79927dc3e9e289c42c22533ce5
Rob Porter <rob@weeverapps.com>
2018-05-14 15:11:38 -0400

    Adds Futures support

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,8 @@
-import { Inquiry, InquiryP, Fail, Pass } from "./index";
+import { Inquiry, InquiryF, InquiryP, Fail, Pass } from "./index";
 import * as R from "ramda";
 import "jasmine";
 import { Maybe } from "simple-maybe";
+import Future from "fluture";
 
 const oldEnough = (a: any) =>
     a.age > 13 ? Pass(["old enough"]) : Fail(["not old enough"]);
@@ -30,57 +31,9 @@ function resolveAfter1Second(x: any) {
     });
 }
 
+const resolveAfter1SecondF = (x: any) => Future.after(1000, Pass("passed"));
+
 describe("The module", () => {
-    // note that while these pass, to use this monad as intended
-    // one should never call Inquiry() directly
-    // this first set of tests take values that aren't of the type expected
-    // @todo convert to type expected
-    it("should satisfy the first monad law of left identity", () => {
-
-        // @ts-ignore
-        const g = (n: number) => Inquiry(n + 1);
-        // @ts-ignore
-        const f = (n: number) => Inquiry(n * 2);
-
-        // 1. unit(x).chain(f) ==== f(x)
-
-        // @ts-ignore
-        const leftIdentity1 = Inquiry(1).chain(f);
-        const leftIdentity2 = f(1);
-
-        expect(leftIdentity1.join()).toEqual(leftIdentity2.join());
-    });
-
-    it("should satisfy the second monad law of right identity", () => {
-
-        // @ts-ignore
-        const rightIdentity1 = Inquiry(2).chain(Inquiry);
-        // @ts-ignore
-        const rightIdentity2 = Inquiry(2);
-
-        // 2. m.chain(unit) ==== m
-        expect(rightIdentity1.join()).toEqual(rightIdentity2.join());
-    });
-
-    it("should satisfy the third monad law of associativity", () => {
-
-        // @ts-ignore
-        const g = n => Inquiry(n + 1);
-        // @ts-ignore
-        const f = n => Inquiry(n * 2);
-
-        // 3. m.chain(f).chain(g) ==== m.chain(x => f(x).chain(g))
-
-        // @ts-ignore
-        const associativity1 = Inquiry(3)
-            .chain(g)
-            .chain(f);
-
-        // @ts-ignore
-        const associativity2 = Inquiry(3).chain(x => g(x).chain(f));
-
-        expect(associativity1.join()).toEqual(associativity2.join());
-    });
 
     it("should be able to make many checks, including async ones, and run a conclude and return the subject unchanged", () => {
         return (InquiryP as any)
@@ -190,7 +143,7 @@ describe("The module", () => {
                 // clearing the existing failure, it will not appear at the end
                 // this is not a practice example, usually one one do some kind of exit
                 x.fail = Fail([]);
-                return Inquiry(x);
+                return Inquiry.subject(x);
             })
             .inquire(findHeight)
             .inquire(nameSpelledRight)
@@ -234,7 +187,7 @@ describe("The module", () => {
                 // clearing the existing failure, it will not appear at the end
                 // this is not a practice example, usually one one do some kind of exit
                 x.pass = Pass([]);
-                return Inquiry(x);
+                return Inquiry.subject(x);
             })
             .inquire(findHeight)
             .inquire(nameSpelledRight)
@@ -266,14 +219,30 @@ describe("The module", () => {
             .inquire(nameSpelledRight)
             .inquire(hasRecords)
             .inquire(mathGrade)
-            .faulted(
-                (x: any) => {
-                    expect(x.inspect()).toBe(
-                        "Fail(not old enough,Name wasn't spelled correctly,Failed at math)"
-                    );
-                    return x;
-                }
-            );
+            .faulted((x: any) => {
+                expect(x.inspect()).toBe(
+                    "Fail(not old enough,Name wasn't spelled correctly,Failed at math)"
+                );
+                return x;
+            });
+    });
+
+    it("should be able to make many checks, including async ones, and run a faulted unwrap", () => {
+        return (InquiryF as any)
+            .subject({ name: "test", age: 10, description: "blah" })
+            .inquire(oldEnough)
+            .inquire(findHeight)
+            .inquire(resolveAfter1SecondF)
+            .inquire(nameSpelledRight)
+            .inquire(hasRecords)
+            .inquire(mathGrade)
+            .faulted((x: any) => {
+                expect(x.inspect()).toBe(
+                    "Fail(not old enough,Name wasn't spelled correctly,Failed at math)"
+                );
+                return x;
+            });
+        //console.log('x', x);
     });
 
     it("should be able to make many checks, including async ones, and run a cleared unwrap when all passes", () => {
@@ -283,13 +252,16 @@ describe("The module", () => {
             .inquire(findHeight)
             .inquire(resolveAfter1Second)
             .inquire(hasRecords)
-            .faulted(
-                (x: any) => {
-                    expect(x.inspect()).toBe(
-                        "Fail(not old enough,Name wasn't spelled correctly,Failed at math)"
-                    );
-                    return x;
-                }
-            );
+            .faulted((x: any) => {
+                expect(x.inspect()).toBe(
+                    "Fail(not old enough,Name wasn't spelled correctly,Failed at math)"
+                );
+                return x;
+            });
+    });
+
+    // due to old prototype method
+    it("should not have prototype pollution", () => {
+        expect(InquiryP.subject === InquiryF.subject).toBe(false);
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Maybe } from "simple-maybe";
+import { Future } from "fluture";
 
 const IOU = (x: any): IOUMonad => ({
     map: (f: Function) => IOU(f(x)),
@@ -75,6 +76,8 @@ const Fail = (x: any): FailMonad => ({
 const Inquiry = (x: Inquiry): InquiryMonad => ({
     // Inquire: core method
     inquire: (f: Function) => {
+        console.log("inquire");
+
         const inquireResponse = f(x.subject.join());
         return inquireResponse.isFail ||
             inquireResponse.isPass ||
@@ -120,7 +123,7 @@ const Inquiry = (x: Inquiry): InquiryMonad => ({
         }),
 
     // standard Monad methods
-    map: (f: Function): Inquiry => (Inquiry as any).of(f(x)), // cast required for now
+    map: (f: Function): Inquiry => (Inquiry as any).subject(f(x)), // cast required for now
     ap: (y: Monad) => y.map(x),
     chain: (f: Function) => f(x),
     join: (): Inquiry => x,
@@ -171,12 +174,183 @@ const Inquiry = (x: Inquiry): InquiryMonad => ({
     //@todo determine if we could zip "in order of tests"
 });
 
+const exportInquiry = {
+    subject: (x: any) =>
+    x.isInquiry
+        ? x
+        : Inquiry({
+              subject: Maybe.of(x),
+              fail: Fail([]),
+              pass: Pass([]),
+              iou: IOU([]),
+              informant: (_: any) => _
+          })
+};
+
+const buildInqF = (x: any) => (vals: Array<any>) =>
+    vals.reduce((acc, cur) => cur.answer(x, "reduced", InquiryF), x);
+
+const InquiryF = (x: Inquiry): InquiryMonad => ({
+    // Inquire: core method
+    inquire: (f: Function) => {
+        console.log("inquireF");
+        const inquireResponse = f(x.subject.join());
+        const syncronousResult = (response: any) =>
+            response.isFail || response.isPass || response.isInquiry
+                ? response.answer(x, f.name, InquiryF)
+                : Pass(response);
+
+        return inquireResponse instanceof Future
+            ? InquiryF({
+                  subject: x.subject,
+                  fail: x.fail,
+                  pass: x.pass,
+                  iou: x.iou.concat(IOU([inquireResponse])),
+                  informant: x.informant
+              })
+            : syncronousResult(inquireResponse);
+    },
+
+    // Informant: for spying/logging/observable
+    informant: (f: Function) =>
+        InquiryF({
+            // @todo accept array of functions instead, or have a plural version
+            subject: x.subject,
+            iou: x.iou,
+            fail: x.fail,
+            pass: x.pass,
+            informant: f
+        }),
+
+    inspect: (): string =>
+        `InquiryF(${x.fail.inspect()} ${x.pass.inspect()} ${x.iou.inspect()}`,
+
+    // Flow control: swap left/right pass/fail (iou is untouched)
+    swap: (): InquiryMonad =>
+        InquiryF({
+            subject: x.subject,
+            iou: x.iou,
+            fail: Fail(x.pass.join()),
+            pass: Pass(x.fail.join()),
+            informant: x.informant
+        }),
+
+    // Mapping across both branches
+    unison: (
+        f: Function
+    ): InquiryMonad => // apply a single map to both fail & pass (e.g. sort), iou untouched
+        InquiryF({
+            subject: x.subject,
+            iou: x.iou,
+            fail: Fail(f(x.fail.join())),
+            pass: Pass(f(x.pass.join())),
+            informant: x.informant
+        }),
+
+    // Standard monad methods - note that while these work, remember that `x` is a typed Object
+    map: (f: Function): Inquiry => (InquiryF as any).subject(f(x)), // cast required for now
+    ap: (y: Monad) => y.map(x),
+    chain: (f: Function) => f(x),
+    join: (): Inquiry => x,
+
+    // execute the provided function if there are failures, else continue
+    breakpoint: (f: Function) => (x.fail.join().length ? f(x) : InquiryF(x)),
+
+    // execute the provided function if there are passes, else continue
+    milestone: (f: Function) => (x.pass.join().length ? f(x) : InquiryF(x)),
+
+    // internal method: execute informant, return new InquiryF() based on updated results
+    answer: (i: Inquiry, n: string, _: Function): InquiryMonad => {
+        i.informant([n, InquiryF(x)]);
+        return InquiryF({
+            subject: i.subject,
+            iou: i.iou,
+            fail: i.fail.concat(x.fail),
+            pass: i.pass.concat(x.pass),
+            informant: i.informant
+        });
+    },
+
+    // Unwrapping methods: all return Promises, all complete outstanding IOUs
+
+    // Unwraps the Inquiry after ensuring all IOUs are completed
+    conclude: (f: Function, g: Function): Future<any, any> =>
+        Future.parallel(Infinity, x.iou.join())
+            .map(buildInqF(x))
+            .map((i: any) => (i.isInquiry ? i.join() : i))
+            .fork(console.error, (y: any) => ({
+                subject: y.subject,
+                iou: y.iou,
+                fail: f(y.fail),
+                pass: g(y.pass),
+                informant: y.informant
+            })),
+
+    // If no fails, handoff aggregated passes to supplied function; if fails, return existing InquiryF
+    cleared: async (f: Function): Promise<InquiryMonad | Array<any>> =>
+        Promise.all(x.iou.join())
+            .then(buildInqF(x))
+            .then(i => (i.isInquiry ? i.join() : i))
+            .then(y => (y.fail.isEmpty() ? f(y.pass) : InquiryF(y)))
+            .catch(err => console.error("err", err)),
+
+    // If fails, handoff aggregated fails to supplied function; if no fails, return existing InquiryF
+    faulted: async (f: Function): Promise<InquiryMonad | Array<any>> =>
+        Future.parallel(Infinity, x.iou.join())
+            .map(buildInqF(x))
+            .map((i: any) => (i.isInquiry ? i.join() : i))
+            .fork(
+                console.error,
+                (y: any) => (y.fail.isEmpty() ? InquiryF(y) : f(y.fail))
+            ),
+
+    // Take left function and hands off fails if any, otherwise takes left function and hands off passes to that function
+    fork: async (f: Function, g: Function): Promise<Array<any>> =>
+        Promise.all(x.iou.join())
+            .then(buildInqF(x))
+            .then(i => (i.isInquiry ? i.join() : i))
+            .then(y => (y.fail.join().length ? f(y.fail) : g(y.pass))),
+
+    // return a Promise containing a merged fail/pass resultset array
+    zip: async (f: Function): Promise<Array<any>> =>
+        Promise.all(x.iou.join())
+            .then(buildInqF(x))
+            .then(i => (i.isInquiry ? i.join() : i))
+            .then(y => f(y.fail.join().concat(y.pass.join()))),
+
+    // await all IOUs to resolve, then return a new Inquiry
+    // @todo: awaitP, awaitF that return an InquiryP or InquiryF
+    await: async (): Promise<InquiryMonad> =>
+        Promise.all(x.iou.join())
+            .then(buildInqF(x))
+            .then(i => (i.isInquiry ? i.join() : i))
+            .then(y => Inquiry(y)),
+
+    isInquiry: true
+    //@todo determine if we could zip "in order of tests"
+});
+
+const exportInquiryF = {
+    subject: (x: any) =>
+        x.isInquiry
+            ? x
+            : InquiryF({
+                  subject: Maybe.of(x),
+                  fail: Fail([]),
+                  pass: Pass([]),
+                  iou: IOU([]),
+                  informant: (_: any) => _
+              })
+};
+
 const buildInq = (x: any) => (vals: Array<any>) =>
     vals.reduce((acc, cur) => cur.answer(x, "reduced", InquiryP), x);
 
 const InquiryP = (x: Inquiry): InquiryMonad => ({
     // Inquire: core method
     inquire: (f: Function) => {
+        console.log("inquireP");
+
         const inquireResponse = f(x.subject.join());
         const syncronousResult = (response: any) =>
             response.isFail || response.isPass || response.isInquiry
@@ -204,6 +378,7 @@ const InquiryP = (x: Inquiry): InquiryMonad => ({
             pass: x.pass,
             informant: f
         }),
+
     inspect: (): string =>
         `InquiryP(${x.fail.inspect()} ${x.pass.inspect()} ${x.iou.inspect()}`,
 
@@ -230,7 +405,7 @@ const InquiryP = (x: Inquiry): InquiryMonad => ({
         }),
 
     // Standard monad methods - note that while these work, remember that `x` is a typed Object
-    map: (f: Function): Inquiry => (InquiryP as any).of(f(x)), // cast required for now
+    map: (f: Function): Inquiry => (InquiryP as any).subject(f(x)), // cast required for now
     ap: (y: Monad) => y.map(x),
     chain: (f: Function) => f(x),
     join: (): Inquiry => x,
@@ -309,32 +484,24 @@ const InquiryP = (x: Inquiry): InquiryMonad => ({
     //@todo determine if we could zip "in order of tests"
 });
 
-Inquiry.constructor.prototype["of"] = (x: any) =>
-    x.isInquiry
-        ? x
-        : Inquiry({
-              subject: Maybe.of(x),
-              fail: Fail([]),
-              pass: Pass([]),
-              iou: IOU([]),
-              informant: (_: any) => _
-          });
+const exportInquiryP = {
+    subject: (x: any) =>
+        x.isInquiry
+            ? x
+            : InquiryP({
+                  subject: Maybe.of(x),
+                  fail: Fail([]),
+                  pass: Pass([]),
+                  iou: IOU([]),
+                  informant: (_: any) => _
+              })
+};
 
-// @ts-ignore
-Inquiry.constructor.prototype["subject"] = Inquiry.of;
-
-InquiryP.constructor.prototype["of"] = (x: any) =>
-    x.isInquiry
-        ? x
-        : InquiryP({
-              subject: Maybe.of(x),
-              fail: Fail([]),
-              pass: Pass([]),
-              iou: IOU([]),
-              informant: (_: any) => _
-          });
-
-// @ts-ignore
-InquiryP.constructor.prototype["subject"] = InquiryP.of;
-
-export { Inquiry, InquiryP, Fail, Pass };
+export {
+    exportInquiry as Inquiry,
+    exportInquiryF as InquiryF,
+    exportInquiryP as InquiryP,
+    Fail,
+    Pass,
+    IOU
+};
